11d9da98e9ac6f58465861b1f1cd3bb916a0f9ca
Hope-Crystal <hope.orobosa.henry@gmail.com>
2021-05-27 09:22:26 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function palindrome(str) {
     let re = /[\W_]/g
-    let lowRegStr = str.toLowerCase().replace(re, '')
-    let reverseStr = lowRegStr.split('').reverse().join('')
-    console.log(lowRegStr, reverseStr)
-    return reverseStr === lowRegStr
+    let toLowerCase = str.toLowerCase().replace(re, '')
+    let reverseStr = toLowerCase.split('').reverse().join('')
+    console.log(toLowerCase, reverseStr)
+    return reverseStr === toLowerCase
 }
 console.log(palindrome("A man, a plan, a canal. Panama"))
 console.log(palindrome("Race car"))
