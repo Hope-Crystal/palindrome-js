67e7491ad11be8bc94cef28117cb3af68dc0f933
Hope-Crystal <hope.orobosa.henry@gmail.com>
2021-05-27 08:55:18 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function palindrome(str) {
-    var re = /[\W_]/g;
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    console.log(lowRegStr, reverseStr)
     return reverseStr === lowRegStr;
 }
 console.log(palindrome("A man, a plan, a canal. Panama"));
 console.log(palindrome("madam"))
-console.log(palindrome("racecar RaceCar rACEcAR"))
+console.log(palindrome("racecar"))
 console.log(palindrome("eat"))
